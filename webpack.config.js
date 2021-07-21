17485b8a98d06a3b38e44ba3bf96f6002b3da652
rajey <rajeyw@gmail.com>
2021-07-22 01:35:27 +0300

    convert all implementation to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,18 @@
-const path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const TypedocWebpackPlugin = require('typedoc-webpack-plugin');
-const { env } = require('yargs').argv;
-const package = require('./package.json');
+const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+// const TypedocWebpackPlugin = require("typedoc-webpack-plugin");
+const { env } = require("yargs").argv;
+const package = require("./package.json");
 
-const isProduction = env === 'prod';
+const isProduction = env === "prod";
 const libraryName = package.name;
 
 module.exports = {
-  mode: isProduction ? 'production' : 'development',
-  entry: './lib/src/index.ts',
-  devtool: 'inline-source-map',
+  mode: isProduction ? "production" : "development",
+  entry: "./lib/src/index.ts",
+  devtool: "inline-source-map",
   optimization: {
     minimizer: [new UglifyJsPlugin()],
   },
@@ -20,43 +20,45 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: 'index.js',
+    filename: "index.js",
     library: libraryName,
-    path: path.resolve(__dirname, 'dist'),
-    libraryTarget: 'umd',
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "umd",
     umdNamedDefine: true,
     globalObject: "typeof self !== 'undefined' ? self : this",
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new CopyPlugin([
-      { from: 'lib/package.json', to: '' },
-      { from: 'lib/*.md', to: '', flatten: true },
-      { from: 'LICENSE', to: '' },
-    ]),
-    new TypedocWebpackPlugin(
-      {
-        name: 'Typescript Library',
-        mode: 'file',
-        out: '../docs',
-        theme: 'default',
-        exclude: ['**/*.spec.ts'],
-        includeDeclarations: false,
-        ignoreCompilerErrors: true,
-        excludeExternals: true,
-        excludeNotExported: true,
-        excludePrivate: true,
-      },
-      './lib'
-    ),
+    new CopyPlugin({
+      patterns: [
+        { from: "lib/package.json", to: "", force: true },
+        { from: "lib/*.md", to: "[name][ext]", force: true },
+        { from: "LICENSE", to: "", force: true },
+      ],
+    }),
+    // new TypedocWebpackPlugin(
+    //   {
+    //     name: "Typescript Library",
+    //     mode: "file",
+    //     out: "../docs",
+    //     theme: "default",
+    //     exclude: ["**/*.spec.ts"],
+    //     includeDeclarations: false,
+    //     ignoreCompilerErrors: true,
+    //     excludeExternals: true,
+    //     excludeNotExported: true,
+    //     excludePrivate: true,
+    //   },
+    //   "./lib"
+    // ),
   ],
 };
